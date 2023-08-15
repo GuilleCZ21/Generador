f1d60c407fe8092fbf9942e62647f5b36aa2ee6c
GuilleCZ21 <128748501+GuilleCZ21@users.noreply.github.com>
2023-08-15 17:54:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+/* Este es el listado de la información de las cartas  */
+const listadoCartas = [
+    {
+        id: 1,
+        nombre: "Lorena",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 2,
+        nombre: "Nicolas",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 3,
+        nombre: "Cristian",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 4,
+        nombre: "Jeronimo",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 5,
+        nombre: "Lucia",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 6,
+        nombre: "Ignacio",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 7,
+        nombre: "Gaston",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+    {
+        id: 8,
+        nombre: "Tomas",
+        imagen: "./imagenes/gato-programador.webp",
+        descripcion: "",
+    },
+]
+
+
+/* Selección de los NODOS HTML (etiquetas/elementos html) */
+const contenedorCartas = document.querySelector(".listadoCartas")
+
+/* Función que recorre el listado de las cartas */
+function generarCartas(){
+    contenedorCartas.innerHTML = ""
+    listadoCartas.forEach(
+        (carta) => contenedorCartas.innerHTML += `
+                <article class="card">
+                    <img src=${carta.imagen} alt="Imagen del proyecto">
+                    <div class="info">
+                      <h2>${carta.nombre}</h2>
+                      <p>${carta.descripcion}</p>
+                    </div>
+                </article>`
+    )
+}
+
+
+function agregarCartas() {
+    const nombre = prompt("Nombre")
+    const imagen = prompt ("Imagen")
+    const descripción = prompt ("Descripción")
+
+    const carta = {nombre,imagen,descripción}
+
+    listadoCartas.push (carta)
+    generarCartas()
+}
+generarCartas()
